8a5292be33aa74e93df6b33189d38cac27d33e5d
SayYee <717349026@qq.com>
2021-05-21 19:54:57 +0800

    进行pkm系统查询功能的微基准测试

---
+++ tbp-core/src/test/java/com/sayyi/software/tbp/core/PkmPerformanceTest.java
@@ -0,0 +1,41 @@
+package com.sayyi.software.tbp.core;
+
+import com.sayyi.software.tbp.core.facade.DefaultPkmFunction;
+import com.sayyi.software.tbp.core.facade.PkmFunction;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * @author xuchuang
+ * @date 2021/5/21
+ */
+public class PkmPerformanceTest {
+
+    @Test
+    public void test() throws Exception {
+        long startTime = System.currentTimeMillis();
+
+        String rootPath = TestConstant.ROOT_PATH;
+
+        String storeDir = rootPath + "store";
+        String snapDir = rootPath + "snap";
+        int treeRetainNum = 10;
+
+        FileManager fileManager = new FileManager(storeDir);
+        MetadataFunction metadataFunction = new MetadataManager();
+        TagTreeFunction tagTreeManager = new TagTreeManager(snapDir, treeRetainNum);
+        DbFunction dbFunction = new FileBasedDbManager(snapDir);
+        PkmService pkmService = new PkmServiceImpl(fileManager, metadataFunction, tagTreeManager, dbFunction);
+        PkmFunction pkmFunction = new DefaultPkmFunction(pkmService);
+
+        for (int i = 0; i < 1000000; i++) {
+            String filename = "PkmPerformanceTest-test-" + i + ".md";
+            Set<String> set = Collections.singleton("PkmPerformanceTest-" + i);
+            pkmFunction.create(filename, set);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("time cost: " + (endTime - startTime));
+    }
+}
---
+++ tbp-core/src/test/java/com/sayyi/software/tbp/core/TestConstant.java
@@ -0,0 +1,10 @@
+package com.sayyi.software.tbp.core;
+
+/**
+ * @author xuchuang
+ * @date 2021/5/21
+ */
+public class TestConstant {
+
+    public static final String ROOT_PATH = "/Users/xuchuang/Desktop/Temp/tbp-test/";
+}
